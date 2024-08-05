eeedd33d633ea1d2964be771624cfe4ecd40785f
maristefer <mirtilm.eva@gmail.com>
2024-08-05 16:29:06 +0000

    modif bouton connexion de nav et mise en place menu-burger

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBurger = document.querySelector('.menu-burger');
+    const menu = document.querySelector('.menu');
+
+    menuBurger.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
