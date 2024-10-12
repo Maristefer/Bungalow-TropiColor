d8be73dc2f016b46dbd5abdcb94f26db700cfd35
maristefer <mirtilm.eva@gmail.com>
2024-10-12 14:56:10 +0000

    modif models users.php selon BD

--- models/User.php
+++ models/User.php
@@ -4,7 +4,7 @@ class User
 {
     private ? int $id =null;
 
-    public function __construct(private string $email, private string $password, private string $role)
+    public function __construct(private string $last_name, private string $first_name, private DateTime $date_of_birth, private string $email, private string $password, private int $adress_id, private int $phone, private DateTime $created_at, private string $role)
     {
         
     }
@@ -17,6 +17,39 @@ class User
     public function setId(?int $id): void
     {
         $this -> id = $id;
+    }
+    
+    public function getLast_name(): string
+    {
+        return $this->last_name;
+    }
+
+    public function setLast_name(string $last_name): void
+    {
+        $this->last_name = $last_name;
+
+    }
+    
+    public function getFirst_name(): string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirst_name(string $first_name): void
+    {
+        $this->first_name = $first_name;
+
+    }
+    
+    public function getDate_of_birth(): DateTime
+    {
+        return $this->date_of_birth;
+    }
+
+    public function setDate_of_birth(DateTime $date_of_birth): void
+    {
+        $this->date_of_birth = $date_of_birth;
+
     }
 
     public function getEmail(): string
@@ -38,6 +71,36 @@ class User
     public function setPassword(string $password): void
     {
         $this->password = $password;
+    }
+    
+    public function getAddress_id(): int
+    {
+        return $this->address_id;
+    }
+
+    public function setAddress_id(int $address_id): void
+    {
+        $this->address_id = $address_id;
+    }
+
+    public function getPhone(): int
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(int $phone): void
+    {
+        $this->phone = $phone;
+    }
+    
+    public function getCreated_at(): DateTime
+    {
+        return $this->created_at;
+    }
+
+    public function setCreated_at(DateTime $created_at): void
+    {
+        $this->created_at = $created_at;
     }
 
     public function getRole(): string
